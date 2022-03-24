c19dba687e6373a8dc64494ad1eb2d046ed4b182
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2022-03-24 16:30:19 -0700

    Add a dependency on purview-scanning

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import PurviewCatalog from "@azure-rest/purview-catalog";
+import PurviewScanning from "@azure-rest/purview-scanning";
 
-const client = PurviewCatalog("https://foo.com", {} as any);
+const client = PurviewCatalog("https://example.org", {} as any);
 const result = await client.path("/atlas/v2/glossary").get();
 console.log(result.status);
+
+
+const scanning = PurviewScanning("https://example.org", {} as any);
+const re = await scanning.path("/azureKeyVaults").get();
+console.log(re.status);
